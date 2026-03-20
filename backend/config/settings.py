e3647c82eeb2d5a7ce450a049beb2101c9e3b9c3
Humberto <ramoshumbertoa6@gmail.com>
2026-03-20 13:45:13 -0400

    assets

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -198,7 +198,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 # Directorio donde `collectstatic` pondrá todos los archivos estáticos para producción.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
